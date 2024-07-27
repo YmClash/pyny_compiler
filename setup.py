e68cbf5a643d154bd6b271298b97522b13c592de
y_mc <momo.toure27@gmail.com>
2024-07-28 00:56:00 +0200

    pyny compiler

---
+++ setup.py
@@ -0,0 +1,16 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+
+
+
+setup(
+    name='pyny_compiler',
+    version='0.0.1',
+    description='Python basoc compiler',
+    url= 'https://github.com/YmClash/pyny_compiler/',
+    author="YmC",
+    packages= find_packages(),
+
+)
